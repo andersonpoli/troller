8ad01f3e618e3d4b466a7a5fceda9eb7f6f76cea
Anderson Poli <anderson.poli@abril.com.br>
2018-11-04 03:43:06 -0200

    finish

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,54 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild   } from '@angular/core';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('mySlider') mySlider: any;
 
-  constructor(public navCtrl: NavController) {
+  timerId;
 
-  }
+  slides =  [
+             {
+               "img": "assets/imgs/tablet-troller-bombeiro-capa.jpg",
+             },
+             {
+               "img": "assets/imgs/tablet-troller-bombeiro-1.jpg",
+             },
+             {
+               "img": "assets/imgs/tablet-troller-bombeiro-2.jpg",
+             },
+             {
+               "img": "assets/imgs/tablet-troller-bombeiro-3.jpg",
+             },
+             {
+               "img": "assets/imgs/tablet-troller-bombeiro-4.jpg",
+             }
+           ]
 
-}
+    constructor(){
+    }
+
+    slideNext(){
+       this.mySlider.slideNext();
+    }
+
+    slidePrev(){
+      this.mySlider.slidePrev();
+    }
+
+    goToStart(){
+       this.mySlider.slideTo(1);
+    }
+
+    slideChanged(){
+      if(this.timerId){
+        clearTimeout(this.timerId);
+      }
+      this.timerId = setInterval(() => {
+        this.goToStart()
+      }, 5000);
+    }
+   
+ }
